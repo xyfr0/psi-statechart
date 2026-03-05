e3b6a4a7055761a580e756ad24a547f9379b5805
xyfr0 <jabbarirhamabdul0@gmail.com>
2026-03-05 18:36:45 +0700

    add js

---
+++ script.js
@@ -0,0 +1,4 @@
+const submit = document.querySelector('button');
+const forenameInput = document.querySelector('input[placeholder="Enter your forename"]');
+const surnameInput = document.querySelector('input[placeholder="Enter your surname"]');
+const ageInput = document.querySelector('input[placeholder="Enter your age"]');
