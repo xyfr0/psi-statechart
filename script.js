53d30b34af3b480a8fab85ff43333ba1bf3f2ccd
xyfr0 <jabbarirhamabdul0@gmail.com>
2026-03-06 10:28:47 +0700

    edit js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const forename = document.getElementById("forename")
 const surname = document.getElementById("surname")
 const age = document.getElementById("age")
+const alertForm = document.getElementById("alert-form")
 
 function submit(){
-    if(forename.value == '' || surname.value == '' || age.value == ''){
-        document.getElementById("alert-form").style.display = block
-    } else if(forename.value.trim() !== '' || surname.value.trim() !== '' || age.value.trim() !== ''){
-       
-    }
+    if(forename.value.trim() === '' || surname.value.trim() === '' || age.value.trim() === ''){
+        alertForm.className = "text-red-700";
+        alertForm.style.display = "block";
+    } else {
+        alertForm.textContent = "Data diterima";
+        alertForm.className = "text-green-700";
+        alertForm.style.display = "block";
+    }                 
 }
