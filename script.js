92feb1c38d136722d877be435138fc4a7e59b25f
xyfr0 <jabbarirhamabdul0@gmail.com>
2026-03-06 10:21:35 +0700

    edit js and html

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const submit = document.querySelector('button');
-const forenameInput = document.querySelector('input[placeholder="Enter your forename"]');
-const surnameInput = document.querySelector('input[placeholder="Enter your surname"]');
-const ageInput = document.querySelector('input[placeholder="Enter your age"]');
+const forename = document.getElementById("forename")
+const surname = document.getElementById("surname")
+const age = document.getElementById("age")
+
+function submit(){
+    if(forename.value == '' || surname.value == '' || age.value == ''){
+        document.getElementById("alert-form").style.display = block
+    } else if(forename.value.trim() !== '' || surname.value.trim() !== '' || age.value.trim() !== ''){
+       
+    }
+}
